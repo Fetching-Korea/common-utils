4187d2c697ab7ca6c0c602166d1dafd043022114
juunini <juuni.ni.i@gmail.com>
2020-12-22 13:04:31 +0900

    Hotfix retry variable name

--- src/retry.ts
+++ src/retry.ts
@@ -1,12 +1,12 @@
-const retry = (limit: number = 3) => async(fn: Function): Promise<any> => {
-	let error: Error
+const retry = (count: number = 3) => async(fn: Function): Promise<any> => {
+	let error: Error | null = null
 
-	for (let i = 0; i < limit; i++) {
+	for (let i = 0; i < count; i++) {
 		try {
 			const result = await fn()
 			return result
 		} catch (e) {
-			error = e
+			if (error === null) error = e
 		}
 	}
   
