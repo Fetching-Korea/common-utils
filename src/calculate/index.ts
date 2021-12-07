d6550c3b3ad04ffd493477c28b8980e3d7bcb24b
최호승 <32726809+startergate@users.noreply.github.com>
2021-12-07 19:07:15 +0900

    feat: max safe integer 32

--- src/calculate/index.ts
+++ src/calculate/index.ts
@@ -1,7 +1,9 @@
 import * as cut from './cut'
+import * as maxSafeInteger32 from './max-safe-integer32'
 
 export const Calculate = {
 	...cut,
+	...maxSafeInteger32,
 }
 
 export default Calculate
---
+++ src/calculate/max-safe-integer32.ts
@@ -0,0 +1,1 @@
+export const MAX_SAFE_INTEGER32 = Math.pow(2, 31) - 1
